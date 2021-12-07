2f5a4c9c604982c57d855651e13b8a4434d05bc8
Kevaljagani <kevaljagani0119@gmail.com>
2021-12-07 11:34:19 +0530

    final commit

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+module.exports = {
+
+
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id: "*"
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  mocha: {
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+    },
+  },
+};
